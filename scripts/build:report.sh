8246c7f0ad49bff47b9ebc97780074388b10ec2a
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2023-08-13 15:10:15 +0200

    Add intro draft

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -3,6 +3,7 @@ mkdir -p ./docs/report/build &&
 cd docs/report &&
 #export "$(xargs < .env)" &&
 xelatex -interaction=batchmode  -output-directory=build ./report.tex > /dev/null;
+makeglossaries -d build/sections sections/glossary;
 texfot bibtex build/report.aux;     # Whoever wrote bibtex needs to just stop.
 xelatex -interaction=batchmode -output-directory=build ./report.tex  > /dev/null; # Yep. Needed for references.
 texfot xelatex -file-line-error -interaction=nonstopmode  -output-directory=build ./report.tex;   # Yep. Needed for bibtex.
